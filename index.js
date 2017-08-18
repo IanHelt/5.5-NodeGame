81775e0a5fa3ac748b056327172094f754a5fe2e
Ian Helt <ianhelt23@gmail.com>
2017-08-18 18:53:10 -0400

    can't set headers error on win/lose

--- index.js
+++ index.js
@@ -16,52 +16,59 @@ app.use(morgan('combined'));
 app.use(session({
   secret: "chimp",
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
 }));
 
 app.use('/', (req, res, next) => {
 if (!req.session.word){
   let random = Math.floor((Math.random()*(words.length-1)));
-  req.session.word = words.filter(word => word.length > 3)[random];
-  req.session.word.split('');
+  req.session.word = words.filter(word => word.length > 3)[random].split('');
   req.session.hiddenLetter = Array(req.session.word.length).fill('_');
   req.session.guessesLeft = [];
   }
 next();
 });
 
-//.join
-//.fill
-
-
-
 app.get('/', (req, res) => {
+  let winCheckA = req.session.hiddenLetter.join('');
+  let winCheckB = req.session.word.join('');
+  let loseCheck = req.session.guessesLeft.length;
+  if(loseCheck == '5'){
+    res.render('lose', {fullWord: winCheckB});
+  }
+  else if(winCheckA === winCheckB){
+    res.render('win');
+  };
+  console.log(req.session);
   console.log(req.session.word);
   console.log(req.session.hiddenLetter);
   console.log(req.session.guessesLeft);
-  res.render('index', {hiddenLetterArray: req.session.hiddenLetter});
+  res.render('index', {hiddenLetterArray: req.session.hiddenLetter, incorrectGuesses: req.session.guessesLeft});
 });
 
-
-
 app.post('/guess', (req, res) => {
-  console.log(req.body.value);
-  console.log(Boolean(req.session.word.includes(req.body.value)));
-  if (req.session.word.includes(req.body.value)){
-  req.session.word.forEach(function(letter){
-  if (letter == req.body.value){
-    let guessIndex = req.session.word.findIndex(req.body.value);
-    req.session.word.splice(guessIndex, 1);
-    req.session.hiddenLetter.fill(req.body.value, guessIndex, guessIndex + 1);
+
+  if (req.session.word.includes(req.body.guessInput) == true
+  && req.session.hiddenLetter.includes(req.body.guessInput) == false){
+  for (var i = 0; i < req.session.word.length; i++){
+  if (req.session.word[i] == req.body.guessInput){
+    req.session.hiddenLetter.fill(req.body.guessInput, i, i + 1);
+    console.log(i);
       };
-    });
+    };
+    res.redirect('/');
+  }else if (req.session.hiddenLetter.includes(req.body.guessInput)) {
     res.redirect('/');
   }else{
-    req.session.guessesLeft.push('strike');
+    req.session.guessesLeft.push(req.body.guessInput);
     res.redirect('/');
   };
 });
 
+app.post('/restart', (req, res) => {
+
+  res.redirect('/');
+});
 
 
 
